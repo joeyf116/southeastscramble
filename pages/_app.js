55c476c71e9156b59f49b349053c33ce816898e6
Joseph Fitzpatrick <joeyf116@yahoo.com>
2023-04-09 22:26:00 -0400

    Set Meta Tags

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ function Application({ Component, pageProps }) {
         <title>Southeast Scramble</title>
         <link rel="icon" href="/images/smallSES.svg" />
         <meta property="og:image" content="https://myurl.com/images/smallSES.svg" />
+        <meta name="description" content="Sub Only Superfights"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
