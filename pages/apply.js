e7a90c4be8ae7bfc0ca9576d3ca4c6947aa968f0
Joseph Fitzpatrick <joeyf116@yahoo.com>
2023-04-10 12:11:57 -0400

    Remove app

--- pages/apply.js
+++
@@ -1,9 +0,0 @@
-export default function Apply() {
-  return (
-    <div className="container">
-      <iframe className="justify-center h-100 w-100" src="https://docs.google.com/forms/d/e/1FAIpQLSe1Ca5kNUBpJqLD_cLnvf6ghTnR4lFgKoqvwr5MJGf2ZA2GSg/viewform?embedded=true">
-        Loading…
-      </iframe>
-    </div>
-  );
-}
