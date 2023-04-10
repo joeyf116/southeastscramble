842c1c23576565106a50edd80c76bed6c79e2ef0
Joseph Fitzpatrick <joeyf116@yahoo.com>
2023-04-09 20:17:29 -0400

    Title for index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '@styles/globals.css'
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
