6b784202cee37aae3f8bbe4d0af9843662e790a9
Joseph Fitzpatrick <joeyf116@yahoo.com>
2023-04-09 21:43:22 -0400

    Resize logo and icon

--- components/CenteredHero.js
+++ components/CenteredHero.js
@@ -2,10 +2,10 @@ import { Instagram } from "react-bootstrap-icons";
 
 export default function CenteredHero({ title }) {
   return <div className="text-center">
-    <img src="/images/textbranding.svg" alt="Southeast Scramble" width="400" />
+    <img src="/images/textbranding.svg" alt="Southeast Scramble" style={{ maxHeight: 350}} />
     <h3>Launching Soon...</h3>
     <p>You can find us on Instagram to stay up to date!</p>
     <hr />
-    <a style={{ textDecoration: "inherit", color: "inherit" }} href="https://www.instagram.com/sessubonly/"><Instagram size={50} /></a>
+    <a style={{ textDecoration: "inherit", color: "inherit" }} href="https://www.instagram.com/sessubonly/"><Instagram size={30} /></a>
   </div>
 }
