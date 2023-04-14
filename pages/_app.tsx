e1849d8b4497256675994a20a6baa39bfe8b4bcf
Joey Fitzpatrick <joeyf116@yahoo.com>
2023-04-14 11:06:36 -0400

    Remove Unused images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,15 +10,16 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Southeast Scramble</title>
         <meta name="title" content="Southeast Scramble" />
         <meta name="description" content="Sub Only. EBI Rules." />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta
           property="image"
-          content="https://southeastscramble.com/images/MultiColorS_48.png"
+          content="https://southeastscramble.com/SVG/brand/MultiColorVerticalLogoFull.svg"
         />
         <meta property="og:title" content="Southeast Scramble" />
         <meta property="og:description" content="Sub Only. EBI Rules." />
         <meta
           property="og:image"
-          content="https://southeastscramble.com/images/MultiColorS_48.png"
+          content="https://southeastscramble.com/SVG/brand/MultiColorVerticalLogoFull.svg"
         />
       </Head>
       <Component {...pageProps} />
